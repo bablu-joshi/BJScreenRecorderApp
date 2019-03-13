f93ce6704e7504bedca869b547288e4d33d98794
Bablu Joshi <bablujoshi@Bablus-MacBook-Pro.local>
2019-03-13 22:25:29 +0530

    Initial Commit : Creating Basic Skeleton for the Application

---
+++ ScreenRecorder/ScreenRecorder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ScreenRecorder
+//
+//  Created by Bablu Joshi on 13/03/19.
+//  Copyright © 2019 Bablu Joshi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
